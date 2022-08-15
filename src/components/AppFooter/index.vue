698d28aad29a6c20d36871107d4a1187a389f090
mxzawadzki <srebrniki@o2.pl>
2022-08-15 16:52:11 +0200

    Add footer component

---
+++ src/components/AppFooter/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="footer">
+    <p>
+      Challenge from
+      <a href="https://www.frontendmentor.io/challenges" target="_blank"
+        >Frontend Mentor.</a
+      >
+    </p>
+    <p>
+      Coded by
+      <a href="https://github.com/jagodakubicka" target="_blank"
+        >Jagoda Kubicka.</a
+      >
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.footer {
+  margin: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p {
+    font-size: 12px;
+  }
+  a {
+    font-size: 12px;
+    margin: 0 5px;
+    color: $Very-dark-cyan;
+  }
+}
+</style>
