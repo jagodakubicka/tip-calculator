63d59b1afcd1ced8294f6d788d62dbe34a4cc84b
mxzawadzki <srebrniki@o2.pl>
2022-08-15 15:47:51 +0200

    Add logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-logo">
+    <img src="./assets/images/splitter.svg" alt="splitter" />
+  </div>
+</template>
+
+<style lang="scss">
+.app-logo {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 100px 0;
+}
+</style>
