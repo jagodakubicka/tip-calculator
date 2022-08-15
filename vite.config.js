0d12a1282a7ed4553f83cdd7614c95f3da00d8db
mxzawadzki <srebrniki@o2.pl>
2022-08-15 16:51:58 +0200

    Use global scss variables

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  // https://stackoverflow.com/questions/70849338/how-to-use-sass-global-variables-from-other-components-in-vite-vue3
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/styles/main.scss";'
+      }
     }
   }
 })
